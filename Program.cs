66859178771d7204098ac5bc7244d78903feae26
chen <motx2152000@gmail.com>
2023-12-19 23:19:15 +0800

    feat:調整

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Gremlin.Net.Driver;
+using Gremlin.Net.Driver.Remote;
+using Gremlin.Net.Structure;
+using static Gremlin.Net.Process.Traversal.AnonymousTraversalSource;
+using static Gremlin.Net.Process.Traversal.T;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            var endpoint = "localhost";
+            var gremlinServer = new GremlinServer(endpoint, 8182, enableSsl: false);
+
+            var gremlinClient = new GremlinClient(gremlinServer);
+          
+            var remoteConnection = new DriverRemoteConnection(gremlinClient, "g");
+            var g = Traversal().WithRemote(remoteConnection);
+            g.AddV("Person").Property("id", "1").Next();
+            g.AddV("Person").Property("id", "2").Next();
+            g.AddV("Person").Property("id", "3").Next();
+            var output = g.V().Limit<Vertex>(3).ToList();
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("{0}", e);
+        }
+    }
+}
